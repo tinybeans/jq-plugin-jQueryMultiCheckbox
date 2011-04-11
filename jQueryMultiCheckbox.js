1cbfd3fbde8949d4000711c47d2c181f87a81ab3
tinybeans <register@tinybeans.net>
2011-04-12 08:45:04 +0900

    Changed ["] to ['].

--- jQueryMultiCheckbox.js
+++ jQueryMultiCheckbox.js
@@ -68,79 +68,83 @@
 
             $self[op.show]();
 
-            $.data(self, "mcb-lists", checked);
+            $.data(self, 'mcb-lists', checked);
 
             // チェックボックスをクリックしたとき
             function checkboxClick(){
-                var checked = $.data(self, "mcb-lists"),
+                var checked = $.data(self, 'mcb-lists'),
                     $cb = $(this),
                     value = $cb.val();
-                if ($cb.is(":checked")) {
+                if ($cb.is(':checked')) {
                     checked.push(value);
-                    $.data(self, "mcb-lists", checked);
+                    $.data(self, 'mcb-lists', checked);
                     $self.val(checked.join(','));
-                    $cb.closest("label").addClass("mcb-label-checked");
+                    $cb.closest('label').addClass('mcb-label-checked');
                 } else {
                     checked = $.grep(checked, function(v, i){
                         return value == v;
                     }, true);
-                    $.data(self, "mcb-lists", checked);
+                    $.data(self, 'mcb-lists', checked);
                     $self.val(checked.join(','));
-                    $cb.closest("label").removeClass("mcb-label-checked");
+                    $cb.closest('label').removeClass('mcb-label-checked');
                 }
             }
 
             // チェックボックスとラベルを生成
             function makeCheckbox(val, label, count, must){
-                var $cb = $("<input/>").attr({"type":"checkbox","value":val}).addClass("mcb").click(checkboxClick);
-                var $label = $("<label></label>").addClass("mcb-label");
-                var checked = $.data(self, "mcb-lists");
+/*
+                var $cb = $('<input/>').attr({'type':'checkbox','value':val}).addClass('mcb').click(checkboxClick);
+                var $label = $('<label></label>').addClass('mcb-label');
+                var checked = $.data(self, 'mcb-lists');
                 if (count > 0) {
                     checked = $.grep(checked, function(elm,idx){
                         if (val == elm) {
-                            $cb.attr("checked","checked");
-                            $label.addClass("mcb-label-checked");
+                            $cb.attr('checked','checked');
+                            $label.addClass('mcb-label-checked');
                             return false;
                         }
                         return true;
                     });
                 }
                 if (must) {
-                    $cb.attr("checked","checked");
-                    $label.addClass("mcb-label-checked");
+                    $cb.attr('checked','checked');
+                    $label.addClass('mcb-label-checked');
                 }
                 $label.text(label).prepend($cb);
                 $self[op.insert]($container.append($label));
+*/
             }
 
             // ユーザーが追加したラベルを生成
             function makeAddCheckbox(arry){
+/*
                 if (arry.length == 0) return;
                 for (var i = -1,n = arry.length; ++i < n;) {
                     makeCheckbox(arry[i],arry[i],0,true);
                 }
+*/
                 
             }
             // ユーザーが項目を追加できるようにする
             function addCheckbox(){
                 if (!op.add) return;
-                var $cb = $("<input/>")
-                        .attr({"type":"checkbox","value":"","checked":"checked"})
-                        .addClass("mcb")
+                var $cb = $('<input/>')
+                        .attr({'type':'checkbox','value':'','checked':'checked'})
+                        .addClass('mcb')
                         .click(checkboxClick);
-                var $input = $("<input/>")
-                        .attr({"type":"text","value":"+"})
-                        .addClass("mcb-add-input")
+                var $input = $('<input/>')
+                        .attr({'type':'text','value':'+'})
+                        .addClass('mcb-add-input')
                         .focus(function(){
-                            if ($(this).val() === "+") $(this).val("");
+                            if ($(this).val() === '+') $(this).val('');
                         })
                         .blur(function(){
-                            if ($(this).val() === "") $(this).val("+");
+                            if ($(this).val() === '') $(this).val('+');
                         })
                         .keydown(function(e){
                             var keycode = e.which || e.keyCode; 
                             if (keycode == 13) {
-                                var value = $(this).val().replace(/^[ 　]*|[ 　]*$/g,"").replace(/[ 　]*:[ 　]*/g,":"), label;
+                                var value = $(this).val().replace(/^[ 　]*|[ 　]*$/g,'').replace(/[ 　]*:[ 　]*/g,':'), label;
                                 if (!value) return;
                                 var obj = value.match(/([^:]+)(:)([^:]+)/);
                                 if (obj) {
@@ -148,26 +152,26 @@
                                     label = obj[3];
                                 }
                                 $(this).hide().before($cb.val(value),label);
-                                $cb.attr({"checked":"checked"}).click().attr({"checked":"checked"});
+                                $cb.attr({'checked':'checked'}).click().attr({'checked':'checked'});
                                 addCheckbox();
                             }
                         });
-                var $label = $("<label></label>")
-                        .addClass("mcb-label mcb-add-label")
+                var $label = $('<label></label>')
+                        .addClass('mcb-label mcb-add-label')
                         .append($input);
                 $($container).append($label);
             }
             // 連想配列のキーを並べ替える
-            function sortHashKey(obj,rule){ // rule = "ascend","descend"
+            function sortHashKey(obj,rule){ // rule = 'ascend','descend'
                 var keys = [], values = [];
                 for (var key in obj) {
                     keys.push(key);
                 }
                 switch (rule) {
-                    case "ascend": 
+                    case 'ascend':
                         keys.sort();
                         break;
-                    case "descend": 
+                    case 'descend':
                         keys.sort();
                         keys.reverse();
                         break;
@@ -179,8 +183,9 @@
             }
 
             // 実行する
-            if (typeof(op.label) == "object") {
-                if (op.sort != "") {
+/*
+            if (typeof(op.label) == 'object') {
+                if (op.sort != '') {
                     var sortRule = sortHashKey(op.label,op.sort);
                     for (var i = -1, n = sortRule.length; ++i < n;) {
                         makeCheckbox(sortRule[i],op.label[sortRule[i]],checked_count,false);
@@ -194,11 +199,11 @@
                 }
                 addCheckbox();
             } else {
-                var checks = (op.label == "") ? $self.attr("title") : op.label,
-                    checks = checks.split(",");
-                    if (op.sort == "ascend") {
+                var checks = (op.label == '') ? $self.attr('title') : op.label,
+                    checks = checks.split(',');
+                    if (op.sort == 'ascend') {
                         checks.sort();
-                    } else if (op.sort == "descend") {
+                    } else if (op.sort == 'descend') {
                         checks.sort();
                         checks.reverse();
                     }
@@ -208,15 +213,16 @@
                 makeAddCheckbox(checked);
                 addCheckbox();
             }
+*/
         });
     };    
     $.fn.multicheckbox.defaults = {
-        show: "hide", // "hide" or "show" 元のテキストフィールドを非表示にするか否か
-        label: "", // カンマ区切りの文字列か{"key1":"value1","key2":"value2"}のハッシュ
-        insert: "before", // "before" or "after"
+        show: 'hide', // 'hide' or 'show' 元のテキストフィールドを非表示にするか否か
+        label: '', // カンマ区切りの文字列か{'key1':'value1','key2':'value2'}のハッシュ
+        insert: 'before', // 'before' or 'after'
         add: false, // ユーザーがチェックボックスを追加できるようにする場合はtrue
         tags: false, // タグデザインを適用する場合はtrue
-        sort: "" // "ascend"（昇順）,"descend"（降順）
+        sort: '' // 'ascend'（昇順）,'descend'（降順）
     };
 
 })(jQuery);
