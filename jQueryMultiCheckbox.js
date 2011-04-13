2cc77ed8b0d9466c9f0e5fd5b102af5a9101f020
tinybeans <register@tinybeans.net>
2011-04-13 18:22:56 +0900

    Clean up the code.

--- jQueryMultiCheckbox.js
+++ jQueryMultiCheckbox.js
@@ -140,77 +140,6 @@
                 }
             }
 
-            // チェックボックスとラベルを生成
-            function makeCheckbox(val, label, count, must){
-/*
-                var $cb = $('<input/>').attr({'type':'checkbox','value':val}).addClass('mcb').click(checkboxClick);
-                var $label = $('<label></label>').addClass('mcb-label');
-                var checked = $.data(self, 'mcb-lists');
-                if (count > 0) {
-                    checked = $.grep(checked, function(elm,idx){
-                        if (val == elm) {
-                            $cb.attr('checked','checked');
-                            $label.addClass('mcb-label-checked');
-                            return false;
-                        }
-                        return true;
-                    });
-                }
-                if (must) {
-                    $cb.attr('checked','checked');
-                    $label.addClass('mcb-label-checked');
-                }
-                $label.text(label).prepend($cb);
-                $self[op.insert]($container.append($label));
-*/
-            }
-
-            // ユーザーが追加したラベルを生成
-            function makeAddCheckbox(arry){
-/*
-                if (arry.length == 0) return;
-                for (var i = -1,n = arry.length; ++i < n;) {
-                    makeCheckbox(arry[i],arry[i],0,true);
-                }
-*/
-                
-            }
-            // ユーザーが項目を追加できるようにする
-            function addCheckbox(){
-                if (!op.add) return;
-                var $cb = $('<input/>')
-                        .attr({'type':'checkbox','value':'','checked':'checked'})
-                        .addClass('mcb')
-                        .click(checkboxClick);
-                var $input = $('<input/>')
-                        .attr({'type':'text','value':'+'})
-                        .addClass('mcb-add-input')
-                        .focus(function(){
-                            if ($(this).val() === '+') $(this).val('');
-                        })
-                        .blur(function(){
-                            if ($(this).val() === '') $(this).val('+');
-                        })
-                        .keydown(function(e){
-                            var keycode = e.which || e.keyCode; 
-                            if (keycode == 13) {
-                                var value = $(this).val().replace(/^[ 　]*|[ 　]*$/g,'').replace(/[ 　]*:[ 　]*/g,':'), label;
-                                if (!value) return;
-                                var obj = value.match(/([^:]+)(:)([^:]+)/);
-                                if (obj) {
-                                    value = obj[1];
-                                    label = obj[3];
-                                }
-                                $(this).hide().before($cb.val(value),label);
-                                $cb.attr({'checked':'checked'}).click().attr({'checked':'checked'});
-                                addCheckbox();
-                            }
-                        });
-                var $label = $('<label></label>')
-                        .addClass('mcb-label mcb-add-label')
-                        .append($input);
-                $($container).append($label);
-            }
             // 連想配列のキーを並べ替える
             function sortHashKey(obj,rule){ // rule = 'ascend','descend'
                 var keys = [], values = [];
@@ -231,39 +160,6 @@
                 }
                 return keys;
             }
-
-            // 実行する
-/*
-            if (typeof(op.label) == 'object') {
-                if (op.sort != '') {
-                    var sortRule = sortHashKey(op.label,op.sort);
-                    for (var i = -1, n = sortRule.length; ++i < n;) {
-                        makeCheckbox(sortRule[i],op.label[sortRule[i]],checked_count,false);
-                    }
-                    makeAddCheckbox(checked);
-                } else {
-                    for (var key in op.label) {
-                        makeCheckbox(key,op.label[key],checked_count,false);
-                    }
-                    makeAddCheckbox(checked);
-                }
-                addCheckbox();
-            } else {
-                var checks = (op.label == '') ? $self.attr('title') : op.label,
-                    checks = checks.split(',');
-                    if (op.sort == 'ascend') {
-                        checks.sort();
-                    } else if (op.sort == 'descend') {
-                        checks.sort();
-                        checks.reverse();
-                    }
-                for (var i = -1, n = checks.length; ++i < n;) {
-                    makeCheckbox(checks[i],checks[i],checked_count,false);
-                }
-                makeAddCheckbox(checked);
-                addCheckbox();
-            }
-*/
         });
     };    
     $.fn.multicheckbox.defaults = {
